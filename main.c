7fe854546c9bde54516bb0b09f829b84e122d659
wacfeld wang <wacfeldwang333@gmail.com>
2022-03-27 01:05:39 -0400

    init2

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// #include <assert.h>
+// #include <ctype.h>
+// #include <errno.h>
+// #include <float.h>
+// #include <limits.h>
+// #include <locale.h>
+// #include <math.h>
+// #include <setjmp.h>
+// #include <signal.h>
+// #include <stdarg.h>
+// #include <stddef.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <time.h>
+#define putd(x) printf(#x ": %d\n", x)
+
+int main()
+{
+
+}
