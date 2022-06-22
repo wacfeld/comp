0e8c02311adc4fcffa9dee502e32398ba48778b6
wacfeld <wacfeldwang333@gmail.com>
2022-06-21 21:24:23 -0400

    fixed preproc.c comment removal bug

--- preproc.c
+++ preproc.c
@@ -6,83 +6,133 @@ typedef int bool;
 #include <stdio.h>
 int lastc = 0;
 
-int mygetchar() {
-  if(lastc) {
+int mygetchar()
+{
+  if(lastc)
+  {
     int temp = lastc;
     lastc = 0;
     return temp;
   }
-  else {
+  else
+  {
     return getchar();
   }
 }
 
-int ungetchar(int c) {
+int ungetchar(int c)
+{
   lastc = c;
 }
 
-int main(void) {
+int main(void)
+{
   int c;
-  
-  while((c = mygetchar()) != EOF) {
-    if(c == '/') {
+
+  while((c = mygetchar()) != EOF)
+  {
+    if(c == '/')
+    {
       c = mygetchar();
-      if(c == '/') {
-        while((c = mygetchar()) != '\n') {
-          if(c == EOF) {
+      if(c == '/')
+      {
+        while((c = mygetchar()) != '\n')
+        {
+          if(c == EOF)
+          {
             return 0;
           }
         }
         putchar('\n');
       }
-      else if(c == '*') {
-        while(1) {
-          while((c = mygetchar()) != '*') {
-            if(c == EOF) {
+      else if(c == '*')
+      {
+        while(1)
+        {
+          while((c = mygetchar()) != '*')
+          {
+            if(c == EOF)
+            {
               return 0;
             }
           }
 
-          if((c = mygetchar()) == '/') {
+          if((c = mygetchar()) == '/')
+          {
             break;
-          } else {
+          }
+          else
+          {
             ungetchar(c);
           }
 
-          if(c == EOF) {
+          if(c == EOF)
+          {
             return 0;
           }
         }
       }
-      else {
+      else
+      {
         putchar('/');
       }
     }
 
-    else {
+    else
+    {
       putchar(c);
     }
 
-    if(c == '"') {
-      while((c = mygetchar()) != EOF) {
+    if(c == '"')
+    {
+      while((c = mygetchar()) != EOF)
+      {
         putchar(c);
-        if(c == '\\') {
-          if((c = mygetchar()) == EOF) {
+        if(c == '\\')
+        {
+          if((c = mygetchar()) == EOF)
+          {
             return 0;
           }
           putchar(c);
         }
-        else if(c == '"') {
-          putchar(c);
+        else if(c == '"')
+        {
           break;
         }
       }
-      if(c == EOF) {
+      if(c == EOF)
+      {
         return 0;
       }
     }
-    
-    if(c == EOF) {
+
+    if(c == '\'')
+    {
+      while((c = mygetchar()) != EOF)
+      {
+        putchar(c);
+        if(c == '\\')
+        {
+          if((c = mygetchar()) == EOF)
+          {
+            return 0;
+          }
+          putchar(c);
+        }
+        else if(c == '\'')
+        {
+          break;
+        }
+      }
+      if(c == EOF)
+      {
+        return 0;
+      }
+    }
+
+    if(c == EOF)
+    {
       break;
     }
   }
